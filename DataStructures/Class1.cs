59f26c0155d1c34183e59fcdecbe42044bf9488a
Jakub Lorenc <kuba.lorenc2@gmail.com>
2018-11-12 12:04:07 +0100

    InitialCommit

---
+++ DataStructures/Class1.cs
@@ -0,0 +1,13 @@
+﻿using DataStructures.Interfaces;
+using System;
+
+namespace DataStructures
+{
+    public class LinkedList : iDataStructure
+    {
+        public T getElement<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataStructures/Interfaces/IDataStructure.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Interfaces
+{
+    interface iDataStructure
+    {
+        T getElement<T>();
+    }
+}
