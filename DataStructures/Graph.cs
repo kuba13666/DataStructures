f4fbe1ebd4b776e9b78fc54c2d9141018da37b88
Jakub Lorenc <kuba.lorenc2@gmail.com>
2019-02-03 18:07:17 +0100

    Graphs

---
+++ DataStructures/Graph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class Graph
+    {
+        public List<Edge> EdgeList;
+        public List<string> VertexList;
+        public Hashtable VertexHashtable;
+        public List<List<int>> AdjacencyMatrix;
+    }
+
+    public class Edge
+    {
+        int EndVertex;
+        int StartVertex;
+        int? Weight;
+        public Edge(int endVertex, int startVertex, int? weight = null)
+        {
+            EndVertex = endVertex;
+            StartVertex = startVertex;
+            Weight = weight;
+        }
+    }       
+}
